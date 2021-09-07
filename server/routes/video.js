f220a9195d35c5b63439ca1d75e2c69eb4d32142
silverz <liisa9601@naver.com>
2021-09-07 17:53:37 +0900

    video detail page 구현

--- server/routes/video.js
+++ server/routes/video.js
@@ -58,6 +58,17 @@ router.get('/getVideos',(req,res)=>{
 
 })
 
+router.get('/getVideoDetail',(req,res)=>{
+    
+    Video.findOne({"_id":req.body.videoId})
+    .populate('writer')
+    .exec((err,videoDetail)=>{
+        if(err) return res.status(400).send(err)
+        return res.status(200).json({success:true, videoDetail})
+    })
+
+})
+
 router.post("/thumbnail", (req, res) => {
 
     //썸네일 생성하고 비디오 러닝타임 가져오기
